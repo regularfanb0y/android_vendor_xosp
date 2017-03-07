c6959b695ca89fb5250290e78db7001de42e8f2f
Calin <nilac8991@gmail.com>
2017-03-07 21:56:10 +0100

    xosp: Remove enhanced NFC
    - No longer in use and it's deprecated
    
    Signed-off-by: Calin <nilac8991@gmail.com>

--- config/nfc_enhanced.mk
+++
@@ -1,3 +0,0 @@
-# This device supports enhanced NFC
-PRODUCT_COPY_FILES += \
-    vendor/xosp/config/permissions/com.cyanogenmod.nfc.enhanced.xml:system/etc/permissions/com.cyanogenmod.nfc.enhanced.xml
