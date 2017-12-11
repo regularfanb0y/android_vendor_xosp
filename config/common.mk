LineageOS/android_vendor_lineage

vendor: Move to PRODUCT_DEFAULT_PROPERTY_OVERRIDES
Change-Id: I96c4da13c0f15795432eef7af24a21e7a5903cd4

config/common.mk
@@ -28,7 +28,7 @@ endif
 
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 # Enable ADB authentication
-ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+PRODUCT_PROPERTY_OVERRIDES += ro.adb.secure=1
 endif
 
 # Backup Tool

regularfanb0y/android_vendor_xosp

config/common.mk
@@ -36,7 +36,7 @@ endif
 
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 # Enable ADB authentication
-ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=1
 endif
 
 #Chromium libs